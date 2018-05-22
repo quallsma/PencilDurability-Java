52d7e0e084d7c3985a7ab66794a8ffc29e280d70
quallsma <quallsma@msu.edu>
2018-05-22 18:34:52 -0400

    Refactoring pencil class to be more readable

--- src/main/java/com/pillartechnology/mqualls/Pencil.java
+++ src/main/java/com/pillartechnology/mqualls/Pencil.java
@@ -3,32 +3,12 @@ package com.pillartechnology.mqualls;
 public class Pencil {
 
 	private static final int DEFAULT_POINT_DURABILITY = 100;
+	private static final String WHITE_SPACE = " ";
 
 	private Integer pointDurability;
 
 	public Pencil() {
 		this.pointDurability = DEFAULT_POINT_DURABILITY;
-	}
-
-	public void write(PaperInterface paper, String text) {
-		String textForPaper = "";
-
-		for(char character : text.toCharArray()){
-			if(pointDurability > 0){
-				textForPaper += character;
-				if(!(character == ' ' || character == '\n'))
-				{
-					if(Character.isUpperCase(character))
-						pointDurability-=2;
-					else
-						pointDurability--;
-				}
-			} else {
-				textForPaper+=" ";
-			}
-		}
-
-		paper.appendText(textForPaper);
 	}
 
 	public Integer getPointDurability() {
@@ -37,5 +17,36 @@ public class Pencil {
 
 	public void setPointDurability(Integer pointDurability) {
 		this.pointDurability = pointDurability;
+	}
+
+	public void write(PaperInterface paper, String text) {
+		paper.appendText(getTextToAppendToPaper(text));
+	}
+
+	private String getTextToAppendToPaper(String text) {
+		StringBuilder textForPaper = new StringBuilder();
+
+		for(char character : text.toCharArray()){
+			if(isPointDurable()){
+				textForPaper.append(character);
+				DegradePointDurability(character);
+			} else {
+				textForPaper.append(WHITE_SPACE);
+			}
+		}
+		return textForPaper.toString();
+	}
+
+	private void DegradePointDurability(char character) {
+		if(characterIsNotWhiteSpaceOrNewLine(character))
+			pointDurability -= Character.isUpperCase(character) ? 2 : 1;
+	}
+
+	private boolean characterIsNotWhiteSpaceOrNewLine(char character) {
+		return !(Character.isWhitespace(character) || character == '\n');
+	}
+
+	private boolean isPointDurable() {
+		return pointDurability > 0;
 	}
 }
